06d368d42cd2c8df491fdb2b33d9cfff7b6265d7
Ben Artin <ben@artins.org>
2019-07-12 14:49:11 -0400

    Refactored worker setup

--- WebUI/server.R
+++ WebUI/server.R
@@ -24,6 +24,7 @@ import::from(lubridate, "%m+%", "%m-%", days)
 import::from(ggplot2, geom_blank, geom_errorbarh)
 
 source("common.R")
+source("worker.R")
 
 plan(multisession)
 
@@ -447,126 +448,6 @@ shinyServer(function(input, output, session) {
     }
   })
 })
-
-# Set up a worker for evaluation of InterventionEvaluatR
-setupWorker = function() {
-  if(getOption("ie.worker.local", TRUE)) {
-    setupLocalWorker()
-  } else {
-    setupRemoteWorker()
-  }
-}
-
-setupLocalWorker = function() {
-  list(local=TRUE)
-}
-
-setupRemoteWorker = function() {
-  # TODO generate ephemeral SSH key
-  
-  machineName = sprintf("iew-%s", UUIDgenerate())
-  
-  # First provision a DigitalOcean droplet
-  analysisStatusDetail("Provisioning DO droplet")
-  check.call(
-    c(
-      sprintf("%s/docker-machine", getOption("ie.webui.docker.bindir")), "create",
-      "--driver", "digitalocean",
-      "--digitalocean-access-token", getOption("ie.digitalocean.access.token"),
-      "--digitalocean-size", getOption("ie.worker.digitalocean-droplet-size", "s-2vcpu-4gb"),
-      "--digitalocean-userdata", "worker/cloud-config.yml",
-      machineName
-    )
-  )
-  
-  workerConfig = check.output(
-    c(
-      sprintf("%s/docker-machine", getOption("ie.webui.docker.bindir")), "config",
-      machineName
-    )
-  )
-  
-  workerIp = check.output(
-    c(
-      sprintf("%s/docker-machine", getOption("ie.webui.docker.bindir")), "ip",
-      machineName
-    )
-  ) %>% trimws()
-  
-  # Copy the worker image
-  analysisStatusDetail("Copying worker image")
-  check.call(
-    c(
-      sprintf("%s/docker-machine", getOption("ie.webui.docker.bindir")), "scp",
-      "worker/image.tar.xz", sprintf("%s:/tmp/worker-image.tar.xz", machineName)
-    )
-  )
-  
-  # Load the worker image into docker
-  analysisStatusDetail("Unarchiving worker image")
-  check.call(
-    c(
-      sprintf("%s/docker-machine", getOption("ie.webui.docker.bindir")), "ssh",
-      machineName, "/usr/bin/unxz", "/tmp/worker-image.tar.xz"
-    )
-  )
-  
-  analysisStatusDetail("Loading worker image")
-  check.call(
-    c(
-      sprintf("%s/docker-machine", getOption("ie.webui.docker.bindir")), "ssh",
-      machineName, sprintf("%s/docker", getOption("ie.worker.docker.bindir")), "load", "--input", "/tmp/worker-image.tar"
-    )
-  )
-  
-  # Make a single-worker cluster 
-  workerCluster = makeClusterPSOCK(
-    workers=workerIp,
-    rshopts=c("-i", "worker/id_rsa", "-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"),
-    user="evaluatr",
-    rscript="/usr/local/bin/Rscript-docker"
-  )
-  
-  list(local=FALSE, cluster=workerCluster, machineName=machineName)
-}
-
-# Generate a future evaluation plan for our worker
-workerPlan = function(worker) {
-  if(worker$local) {
-    localWorkerPlan(worker)
-  } else {
-    remoteWorkerPlan(worker)
-  }
-}
-
-localWorkerPlan = function(worker) {
-  plan()
-}
-
-remoteWorkerPlan = function(worker) {
-  plan(cluster, workers=worker$cluster)
-}
-
-# Shut down the worker
-dismissWorker = function(worker) {
-  if(worker$local) {
-    dismissLocalWorker(worker)
-  } else {
-    dismissRemoteWorker(worker)
-  }
-}
-
-dismissLocalWorker = function(worker) {
-}
-
-dismissRemoteWorker = function(worker) {
-  check.call(
-    c(
-      sprintf("%s/docker-machine", getOption("ie.webui.docker.bindir")), "rm", "--force",
-      worker$machineName
-    )
-  )
-}
 
 analysisStatusDetail <- function(text) {
   print(text)
