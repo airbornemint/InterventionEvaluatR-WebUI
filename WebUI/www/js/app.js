161db13c7261e13c3fc3029faa1430dbe5c84c65
Ben Artin <ben@artins.org>
2019-08-18 23:10:52 -0400

    Switch help when switching tabs

--- WebUI/www/js/app.js
+++ WebUI/www/js/app.js
@@ -1,16 +1,28 @@
+function showHelpSection(section) {
+    var help = $("#help");
+    var newSection = help.find("#help-" + section);
+    help.find(".help-section").not(newSection).removeClass("active");
+    newSection.addClass("active");
+}
+
 $(document).ready(function(){
   // Update help section when setup stepper changes
   $("#steps").on("stepper:selection-changed", function(event, selected) {
-    var help = $("#help");
-    var newSection = help.find("#help-" + selected);
-    help.find(".help-section").not(newSection).removeClass("active");
-    newSection.addClass("active");
+    showHelpSection(selected);
   });
   
   // Help button toggles help content
   $("#help-button").on("click", function(event) {
     $("#page").toggleClass("help-on");
     $(window).trigger("resize"); // Force shiny to resize plots
+  });
+  
+  $('a[data-toggle="tab"]').on("show.bs.tab", function(event) {
+    if ($(event.target).attr("id") == "nav-results-tab") {
+      showHelpSection("results");
+    } else {
+      showHelpSection($("#nav-setup ul.stepper > li.active").attr("id"));
+    }
   });
 });
 
