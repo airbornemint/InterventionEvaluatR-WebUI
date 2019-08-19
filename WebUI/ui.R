12697ffe3f956b8caa13edd9994a60f44d64f907
Ben Artin <ben@artins.org>
2019-08-18 23:10:42 -0400

    Wrote some help

--- WebUI/ui.R
+++ WebUI/ui.R
@@ -210,50 +210,52 @@ md_page(
                     class="help-section card-body",
                     id="help-load",
                     h1("Loading data"),
-                    p("Lorem ipsum")
+                    h2("Stock data"),
+                    p("If you don't have your own data, you can explore InterventionEvaluatR by using a stock dataset."),
+                    h2("Your data"),
+                    p("You can also load your own data into InterventionEvaluatR."),
+                    h3("Data Format"),
+                    p("Your data must be a CSV file with the following columns:"),
+                    tags$ul(
+                        tags$li("One column containing the date of each observation. The date must be in one of the following formats: YYYY-MM-DD, YYYY-DD-MM, MM-DD-YYYY, or DD-MM-YYYY"),
+                        tags$li("One column containing the outcome variable (usually incidence or incidence rate"),
+                        tags$li("If your data contains multiple observations for each date (often, these are age groups), then each observation has to be assigned to a group; the group assigment is in its own column"),
+                        tags$li("If the outcome variable is incidence, then a separate column containing the denominator should be included."),
+                        tags$li("Any other relevant variables you want to include as predictors in the analysis.")
+                    )
                 ),
                 div(
                     class="help-section card-body",
                     id="help-date",
                     h1("Selecting time variable"),
-                    p("Lorem ipsum")
+                    p("Choose which variable in your data contains the date of the observations. Only variables that contain valid dates are shown here.")
                 ),
                 div(
                     class="help-section card-body",
                     id="help-outcome",
                     h1("Selecting outcome"),
-                    p("Lorem ipsum"),
-                    p("Lorem ipsum"),
-                    p("Lorem ipsum"),
-                    p("Lorem ipsum"),
-                    p("Lorem ipsum"),
-                    p("Lorem ipsum"),
-                    p("Lorem ipsum"),
-                    p("Lorem ipsum"),
-                    p("Lorem ipsum"),
-                    p("Lorem ipsum"),
-                    p("Lorem ipsum"),
-                    p("Lorem ipsum"),
-                    p("Lorem ipsum"),
-                    p("Lorem ipsum"),
-                    p("Lorem ipsum"),
-                    p("Lorem ipsum"),
-                    p("Lorem ipsum"),
-                    p("Lorem ipsum"),
-                    p("Lorem ipsum"),
-                    p("Lorem ipsum")
+                    p("Choose which variable in your data contains the outcome variable — usually, incidence or incidence rate."),
+                    p("If your data is grouped (for example, by age), then you must specify which variable lists the group for each observation."),
+                    p("If the outcome variable you selected above is incidence (rather than incidence rate), then you also need to select the denominator variable.")
                 ),
                 div(
                     class="help-section card-body",
                     id="help-periods",
                     h1("Selecting analysis periods"),
-                    p("Lorem ipsum")
+                    p("Specify the month in which the vaccine was introduced."),
+                    p("Also specify how long it took the vaccine to become established in the population. If you are unsure, 24 months is a good initial choice.")
                 ),
                 div(
                     class="help-section card-body",
                     id="help-analysis",
                     h1("Running the analysis"),
-                    p("Lorem ipsum")
+                    p("If you specified a group variable above, then you can choose to exclude some groups from the analysis. The analysis takes several minutes for each group, so excluding unneeded groups is definitely a good idea.")
+                ),
+                div(
+                    class="help-section card-body",
+                    id="help-results",
+                    h1("Interpreting the results"),
+                    p("Lorem ipsum.")
                 )
             )
         )
